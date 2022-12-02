8bf37b72b9ddf175833ebc5c247458da5a7ce6ef
Gary Seip <seipg@merrimack.edu>
2022-12-02 18:49:59 -0500

    Implemented a test to add particles to the scene.

--- dust.js
+++ dust.js
@@ -6,10 +6,21 @@
  * This file implements 
  */
 
- import * as THREE from "http://cs.merrimack.edu/~stuetzlec/three.js-master/build/three.module.js";
+import * as THREE from "http://cs.merrimack.edu/~stuetzlec/three.js-master/build/three.module.js";
 
- class ParticleSimulator
- {
+class Particles
+{
+    constructor(count)
+    {
+        this.geometry = new THREE.OctahedronGeometry(1, 1);
+        this.material = new THREE.MeshPhongMaterial({color: 0xd2b48c});
+
+        this.particles = new THREE.InstancedMesh(this.geometry, this.material, count);
+    }
+}
+
+class ParticleSimulator
+{
     /**
      * The constructor takes in the scene and the camera in order to be able tocreate a list of every non-camera object in 
      * the scene.
@@ -21,7 +32,11 @@
     {
         this.scene = scene;
         this.camera = camera;
-    }
- }
 
- export {ParticleSimulator};
+        this.particles = new Particles(1000);
+
+        this.scene.add(this.particles.particles);
+    }
+}
+
+export {ParticleSimulator};
